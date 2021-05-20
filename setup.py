cd1c32501dde21db7264f82f43ef088354ea7872
John Wason <wason@wasontech.com>
2021-05-19 20:29:27 -0400

    Fix package data

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
+    package_data = {
+        'pyri.sandbox': ['*.robdef','*.js']
+    },
     zip_safe=False,
     install_requires=[
         'pyri-common',
